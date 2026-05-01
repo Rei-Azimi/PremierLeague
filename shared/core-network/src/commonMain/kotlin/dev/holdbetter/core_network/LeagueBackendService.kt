90f5869e0ce2a1314d50bacf178c03a2b6792752
Reihaneh <razi0005@student.monash.edu>
2026-05-01 23:31:54 +1000

    Update backend service URL for LeagueBackendService

--- shared/core-network/src/commonMain/kotlin/dev/holdbetter/core_network/LeagueBackendService.kt
+++ shared/core-network/src/commonMain/kotlin/dev/holdbetter/core_network/LeagueBackendService.kt
@@ -1,7 +1,7 @@
 package dev.holdbetter.core_network
 
 object LeagueBackendService {
-    internal const val CLIENT = "https://premierleagueservice-production.up.railway.app"
+    internal const val CLIENT = "https://premier-league-backend-323733522796.australiasoutheast1.run.app"
 
     object Paths {
         const val STANDINGS = "standings"
